9cb73af0da9862b62c2772d106fbbae0c5259961
MatiasARSdev <105838341+MatiasARSdev@users.noreply.github.com>
2023-02-11 18:29:40 -0300

    reto1

--- notes-react/src/App.jsx
+++ notes-react/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Listado from './components/Listado'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>Hola</h1>
+      <Listado/>
     </div>
   )
 }
---
+++ notes-react/src/components/Listado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Listado = () => {
+  return (
+    <div>
+        <h2>Listado</h2>
+    </div>
+  )
+}
+
+export default Listado
